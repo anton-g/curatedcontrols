00180c46efb1156e2ab0d4e13605215985c2d7b5
= <anton@lyxit.se>
2015-08-11 20:01:52 +0200

    Add ToS and privacy pages

--- app/routes.js
+++ app/routes.js
@@ -15,6 +15,12 @@
     .when('/c/:id', {
       templateUrl: 'app/control/single-control/index.html',
     })
+    .when('/tos', {
+      templateUrl: 'app/tos/index.html',
+    })
+    .when('/privacy', {
+      templateUrl: 'app/privacy/index.html',
+    })
     .otherwise({
       redirectTo: '/'
     });
