5ff0da219b2d464252729db131fff0ce198f4355
= <anton@lyxit.se>
2015-10-14 18:58:47 +0200

    Add static page for Facebook crawler

--- app/core/routes.js
+++ app/core/routes.js
@@ -4,26 +4,29 @@
       .module('curatedcontrols.core')
       .config(routeProvider);
 
-  function routeProvider($routeProvider) {
+  /* @ngInject */
+  function routeProvider($routeProvider, $locationProvider) {
     $routeProvider
-    .when('/', {
-      templateUrl: 'app/index.html',
-    })
-    .when('/c/:id/:name', {
-      templateUrl: 'app/control/control.html',
-    })
-    .when('/c/:id', {
-      templateUrl: 'app/control/control.html',
-    })
-    .when('/tos', {
-      templateUrl: 'app/tos/tos.html',
-    })
-    .when('/privacy', {
-      templateUrl: 'app/privacy/privacy.html',
-    })
-    .otherwise({
-      redirectTo: '/'
-    });
+      .when('/', {
+        templateUrl: 'app/index.html',
+      })
+      .when('/c/:id/:name', {
+        templateUrl: 'app/control/control.html',
+      })
+      .when('/c/:id', {
+        templateUrl: 'app/control/control.html',
+      })
+      .when('/tos', {
+        templateUrl: 'app/tos/tos.html',
+      })
+      .when('/privacy', {
+        templateUrl: 'app/privacy/privacy.html',
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
   }
 
 })();
---
+++ static-page.php
@@ -0,0 +1,48 @@
+<?php
+$SITE_ROOT = "http://www.curatedcontrols.com/";
+
+$jsonData = getData($SITE_ROOT);
+makePage($jsonData, $SITE_ROOT);
+function getData($siteRoot) {
+    $id = $_GET['id'];
+
+    $MyApplicationId = 'j9xiw2SW5WIg3GcFe5L3mJeIX61zKiXqKwdDcwlG';
+    $MyParseRestAPIKey = 'U7Z83FdFHLo9ViIyzg3vMH3Rm7AIqGe9fxgKug4b';
+    $url = 'https://api.parse.com/1/classes/control/'.$id;
+
+    $headers = array(
+        "Content-Type: application/json",
+        "X-Parse-Application-Id: " . $MyApplicationId,
+        "X-Parse-REST-API-Key: " . $MyParseRestAPIKey
+    );
+
+    $handle = curl_init();
+    curl_setopt($handle, CURLOPT_URL, $url);
+    curl_setopt($handle, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+
+    $data = curl_exec($handle);
+    curl_close($handle);
+
+    return json_decode($data);
+}
+
+function makePage($data, $siteRoot) {
+    ?>
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta property="og:title" content="<?php echo $data->name; ?>" />
+            <meta property="og:description" content="<?php echo $data->description; ?>" />
+            <meta property="og:image" content="<?php echo current($data->previews)->url; ?>" />
+            <!-- etc. -->
+        </head>
+        <body>
+            <p><?php echo $data->description; ?></p>
+            <img src="<?php echo current($data->previews)->url; ?>">
+        </body>
+    </html>
+    <?php
+}
+?>
